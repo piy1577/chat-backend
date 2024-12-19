6fd1f9e3bfe3e65b82d0ae0be7eab6287861a0aa
Turbobot <turbobot@vercel.com>
2024-12-19 09:54:27 +0530

    feat(create-turbo): apply official-starter transform

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-require("dotenv").config({ path: "./src/config/.env" });
-
-const connect = require("./src/config/db");
-
-const PORT = process.env.PORT || 3000;
-const server = http.createServer(require("./src/app"));
-require("./src/controller/Chat.controller").socket(server);
-connect(server, PORT);
